92681f7822548fe6a582960cb27b06c447bfedb3
arnauddersion <arnaud.dersion@hotmail.com>
2024-11-16 16:28:09 +0100

    SEGV !

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nalebrun <nalebrun@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:53:50 by nalebrun          #+#    #+#             */
-/*   Updated: 2024/10/22 09:28:15 by nalebrun         ###   ########.fr       */
+/*   Updated: 2024/11/16 16:27:35 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if(!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
